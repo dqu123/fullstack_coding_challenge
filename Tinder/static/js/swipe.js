17233fe196389b875316a7fcd8f24fc9c2d303db
dqu123 <davidqu12345@gmail.com>
2015-09-14 00:38:45 -0400

    Changed cardManager javascript init.

--- Tinder/static/js/swipe.js
+++ Tinder/static/js/swipe.js
@@ -1,62 +1,29 @@
-(function () {})();
-
 // Object to manage Tinder card data.
 var cardManager = (function() {
-    var obj = {};
-    var cards = [];
-    var rightCards = [];
-    var leftCards = [];
+    obj = {};
 
-    obj.init = function(newCards) {
-        cards = newCards;
-    };
-
-    obj.getCards = function() {
-        return cards;
-    }
-
-    obj.renderCard = function(mountNode) {
-        if (cards.length === 0) {
-            return;
-        }
-        var curCard = cards.pop();
-        var cardNode = $(mountNode).append('<div id="cur-card">' + curCard.name +
-            ', ' + curCard.age + '</div>');
-        var mc = new Hammer(document.getElementById('cur-card'), {drag: true});
-
-        mc.on("swipeleft swiperight drag", function(ev) {
+    function addHandler(element) {
+        var mc = new Hammer(element);
+        mc.on("swipeleft swiperight", function(ev) {
             console.log(ev.type);
-            switch (ev.type) {
-            case "drag":
-                var target = ev.target;
-                $(target).css({
-                    'transform': 'translate(' + event.gesture.deltaX + 'px,' +
-                    event.gesture.deltaY + 'px)'});
-                break;
+            $(element).remove();
+            switch(ev.type) {    
             case "swipeleft":
-                $('#cur-card').remove();
-                leftCards.push(curCard);
-                obj.renderCard(mountNode);
                 break;
             case "swiperight":
-                $('#cur-card').remove();
-                rightCards.push(curCard);
                 alert("It's a match!");
-                obj.renderCard(mountNode);
                 break;
             }
         });
-        $('.jumbotron').get(0).addEventListener('touchmove', function(event) {
-            var touch = event.targetTouches[0];
-            touch_log = touch;
-                   
-            // Place element where the finger is
-            event.target.style.left = touch.pageX + 'px';
-            event.target.style.top = touch.pageY + 'px';
-            event.preventDefault();
-        }, false);
     }
 
+    obj.init = function() {
+        array = $('#viewport').find('.stack').children();
+        for (var i = 0; i < array.length; i++) {
+            addHandler(array[i]);
+        }
+    }
+    
     return obj;
 })();
 
