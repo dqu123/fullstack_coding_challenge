4e3793a608e2463fd9147353950a7653b384ca4b
dqu123 <davidqu12345@gmail.com>
2015-09-14 22:37:47 -0400

    Added Tinder header text.

--- Tinder/static/js/swipe.js
+++ Tinder/static/js/swipe.js
@@ -2,25 +2,56 @@
 var cardManager = (function() {
     obj = {};
 
-    function addHandler(element) {
-        var mc = new Hammer(element);
-        mc.on("swipeleft swiperight", function(ev) {
-            console.log(ev.type);
-            $(element).remove();
-            switch(ev.type) {    
-            case "swipeleft":
-                break;
-            case "swiperight":
-                alert("It's a match!");
-                break;
-            }
+    /**
+     * Example backend endpoints
+     * These functions represent backend endpoints
+     * for saving card data, and would be integrated
+     * with the database in a real production setting.
+     */
+    
+    // Backend endpoint
+    function saveSwipe(swipeType, id) {
+        // Not implemented since there is no backend.
+    }
+
+    // Display it's a match!
+    function showMatch() {
+        $('#content').prepend('<img id="its-a-match"' +
+            'src="static/images/its-a-match.png" />').on('click', function() {
+            $('#its-a-match').remove();
         });
     }
 
+    /**
+     * Handles decision and saves it. 
+     * @param {string} swipeType -- "swipeleft" or "swiperight"
+     * @param {string} id -- Card id that
+     */
+    function handleDecision(swipeType, id) {
+        $('#its-a-match').remove();
+        if (swipeType === "swiperight") {
+            showMatch();
+        }
+        saveSwipe(swipeType, id);
+    }
+
+    // Add Hammer.js handlers.
+    function addHandler(element) {
+        var mc = new Hammer(element);
+        mc.on('swipeleft swiperight', function(ev) {
+            console.log(element);
+            var id = element.getAttribute('card_id');
+            $(element).remove();
+            handleDecision(ev.type, id);
+        });
+    }
+
+    // Initializes handlers.
     obj.init = function() {
-        array = $('#viewport').find('.stack').children();
-        for (var i = 0; i < array.length; i++) {
-            addHandler(array[i]);
+        // Array of cards
+        cards = $('#viewport').find('.stack').children();
+        for (var i = 0; i < cards.length; i++) {
+            addHandler(cards[i]);
         }
     }
     
